ada07b8819bd73c6c022ad0748fd7e5259f7d946
esello <esello@aaib.com>
2024-01-06 14:58:50 +0200

    rract query

--- api/middleware/errorHandler.js
+++ api/middleware/errorHandler.js
@@ -1,16 +1,12 @@
-export const createError = (  message ,status) =>
-{
-  const error = new Error()
-  error.message = message
-  error.status = status
-return error
+export const createError = (message, status) => {
+  const error = new Error();
+  error.message = message;
+  error.status = status;
+  console.error(`Created error: ${status} - ${message}`);
+  return error;
+};
 
-}
-
-export const errorHandler = ( err, req, res, next ) =>
-{
-  const errorStatus = err.status || 500
-  const errorMessage = err.message || `Some thing went wrong!. ${err}`
-    return res.status( errorStatus ).send( errorMessage )
-    
-}
+export const errorHandler = (err, req, res, next) => {
+  console.error("Error caught by errorHandler:", err.message);
+  res.status(err.status || 500).send({ error: err.message });
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,6 +3,6 @@ import { login ,register, logout} from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post("/register",register)
-router.post("/login",login)
+router.post("/login", login);
 router.post("/logout",logout)
 export default router;
